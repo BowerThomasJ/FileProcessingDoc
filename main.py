630c0dcf8e378223c4d2598dfb443c836590d818
BowerThomasJ <thbower@my365@bellevue.edu>
2023-08-02 23:51:55 -0500

    Changes to assignment to add functions.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-fileName = input("what is the file name")
-firstName = input("What is your first name")
-address = input("What is your street address name")
-phone = input("what is your phone number")
+def information():
+    fileName = input("What is the file name: ")
+    firstName = input("What is your first name: ")
+    address = input("What is your street address: ")
+    phone = input("What is your phone number: ")
 
-with open(fileName, 'w') as newFile:
-    newFile.write(firstName + ',' + address + ',' + phone)
+    fileSave(fileName, firstName, address, phone)
 
-with open(fileName) as readFile:
-    for line in readFile:
-        print(line)
+
+def fileSave(fileName, firstName, address, phone):
+    with open(fileName, 'w') as newFile:
+        newFile.write(firstName + ',' + address + ',' + phone)
+
+    with open(fileName) as readFile:
+        for line in readFile:
+            print(line)
+
+
+# Call the information() function to start the process
+information()
